39522c921e29c017c2baa1d843b2a2ee003f0538
Luiza <luiza.mustafaeva1997@gmail.com>
2020-09-02 12:55:03 +0500

    MainViewController: work with sliders

---
+++ YourBMI/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  YourBMI
+//
+//  Created by Luiza on 02.09.2020.
+//  Copyright © 2020 Luiza. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height) м"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight) кг"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight/pow(height, 2)
+    }
+}
+
--- YourBMI/ResultViewController.swift
+++ YourBMI/ResultViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ResultViewController.swift
 //  YourBMI
 //
 //  Created by Luiza on 02.09.2020.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
 
 }
-
