57cfb3cdd0f10a92c08a8061eb1491ee74e7a8e5
Luiza <luiza.mustafaeva1997@gmail.com>
2020-09-02 11:46:49 +0500

    Initial Commit

---
+++ YourBMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourBMI
+//
+//  Created by Luiza on 02.09.2020.
+//  Copyright © 2020 Luiza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
